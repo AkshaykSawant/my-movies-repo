63b0afe8ec5c8d2f719573ea21f52a93a5ef793f
AKshaykSawant <akshayksawant18@gmail.com>
2023-11-23 15:50:34 +0530

    Filter Fix

--- src/components/container/MainContent.tsx
+++ src/components/container/MainContent.tsx
@@ -35,9 +35,10 @@ export const MainContent = () => {
     (async () => {
       try {
         setHasMore(true);
+        setYear(2012);
         const response: MovieApiResponse = await axios.get(
           `https://api.themoviedb.org/3/discover/movie?api_key=2dca580c2a14b55200e784d157207b4d&sort_by=popularity.desc
-          &primary_release_year=${year}&page=${1}&vote_count.gte=100&with_genres=${selectedGenre}`
+          &primary_release_year=${2012}&page=${1}&vote_count.gte=100&with_genres=${selectedGenre}`
         );
         if (response.data.results.length === 0) {
           setHasMore(false);
