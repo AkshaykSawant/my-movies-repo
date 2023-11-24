3cb1e75fc6eccbbdbac36233fbea073dfb8d843e
AKshaykSawant <akshayksawant18@gmail.com>
2023-11-24 16:02:58 +0530

    Card stylings

--- src/components/card/MovieCard.tsx
+++ src/components/card/MovieCard.tsx
@@ -13,22 +13,13 @@ const MovieCard: React.FC<Props> = ({ movie, index }) => {
         <h1 className="movie-year">{movie.release_date.split("-")[0]}</h1>
       )}
       <div className="movie-card">
-        <div className="movie-card-title">
-          <h2>{movie.title}</h2>
-        </div>
         <img
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}
         />
         <div className="movie-card-content">
-          <div className="row">
-            <div className="col col-left">Genre</div>
-            <div className="col col-right">{movie.genre_ids}</div>
-          </div>
-          <div className="row">
-            <div className="col col-left">Rating</div>
-            <div className="col col-right">{movie.vote_average}</div>
-          </div>
+          <div className="row row-title">{movie.title}</div>
+          <div className="row row-rating">{movie.popularity}</div>
         </div>
       </div>
     </>
